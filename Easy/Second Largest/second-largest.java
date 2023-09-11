08f8695599d9d24aa8b8dab1a21c8dffa68e4bd6
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2023-09-11 11:32:32 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Second Largest/second-largest.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.stream.Collectors; 
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        // code here
+        int max=Integer.MIN_VALUE;
+        int s_max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                s_max = max;
+                max = arr[i];
+            }else if(arr[i]>s_max && arr[i]<max){
+                s_max = arr[i];
+            }
+        }
+        if(s_max == Integer.MIN_VALUE){
+            return -1;
+        }
+        return s_max;
+    }
+}
