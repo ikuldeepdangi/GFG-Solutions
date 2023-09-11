bcb712dee8cbeabce396ac2884e5c4914aae3771
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2023-09-11 11:12:27 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Largest Element in Array/largest-element-in-array.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.largest(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        int i=0;
+        int j=n-1;
+        int max = arr[0];
+        
+       while(i<=j){
+           if(max<arr[i]){
+               max=arr[i];
+           }
+           if(max<arr[j]){
+               max = arr[j];
+           }
+           
+           i++;
+           j--;
+       }
+       return max;
+    }
+}
