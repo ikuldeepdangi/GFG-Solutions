d64468dce3ed28a7f3b1f5be3fbb5783981f8dfa
Kuldeep Dangi <90251544+kuldeep0912@users.noreply.github.com>
2023-09-13 10:38:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Largest number possible/largest-number-possible.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(a[0]);
+            int S = Integer.parseInt(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findLargest(N, S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+//User function Template for Java
+
+class Solution{
+    static String findLargest(int N, int S){
+        // code here
+        if((S==0&& N>1) || S>9*N)
+           return "-1";
+        String ans="";
+        while(N>0){
+            N--;
+            if(S>0 && S<=9){
+                 ans=ans+S+"";
+                 S=0;
+            }
+            else if(S>9){
+                ans=ans+"9";
+                S-=9;
+            }
+            else if(S==0)
+                ans=ans+"0";
+            
+        }
+      //  System.out.print(ans);
+        return ans+"";
+    }
+}
