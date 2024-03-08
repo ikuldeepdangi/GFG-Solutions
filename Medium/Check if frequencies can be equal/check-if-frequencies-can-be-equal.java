a63bcd62bafd416ba31efb3076e1cc75afea0327
Kuldeep Dangi <90251544+ikuldeepdangi@users.noreply.github.com>
2024-03-08 09:42:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if frequencies can be equal/check-if-frequencies-can-be-equal.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            
+            String input[] = read.readLine().split(" ");
+            String a = input[0];
+            
+            Solution ob = new Solution();
+            if(ob.sameFreq(a)){
+                System.out.println(1);
+            }
+            else{
+                System.out.println(0);
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    boolean sameFreq(String s) {
+        int[] freq = new int[26];
+        
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        
+        if(check(freq)) return true;
+        
+        for(int i = 0;i < 26;i++){
+            if(freq[i] > 0){
+                freq[i]--;
+                
+                if(check(freq)) return true;
+                
+                freq[i]++;
+            }
+        }
+        
+        return false;
+    }
+    boolean check(int[] arr){
+        int n = -1;
+        for(int i = 0;i < arr.length;i++){
+            if(arr[i] > 0){
+                
+                if(n == -1) n = arr[i];
+                else if(arr[i] != n) return false;
+            }
+        }
+        return true;
+    }
+}
